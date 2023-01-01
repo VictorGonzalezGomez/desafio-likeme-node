68ba61c4939c956fcc4004a64633b5d90989d6e1
VictorGonzalezGomez <v.gonzalezgomez.vg@gmail.com>
2023-01-01 20:36:09 -0300

    add: inserting user input into likeme databese

--- database.js
+++ database.js
@@ -8,3 +8,13 @@ const pool = new Pool({
     database: process.env.DATABASE,
     allowExitOnIdle: true,
 })
+
+const insertPost = async (payload) => {
+    const query = "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3) RETURNING *";
+    const values = [payload.titulo, payload.url, payload.descripcion];
+    const result = await pool.query(query, values);
+    console.log("result rows:", result.rows);
+    return result.rows;
+}
+
+module.exports = {insertPost}
