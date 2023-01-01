6707b4f82aa2dca590c55b8304b5baab90dc662a
VictorGonzalezGomez <v.gonzalezgomez.vg@gmail.com>
2023-01-01 17:08:29 -0300

    add: .env config pool in database.js and config server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -7,11 +8,13 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
+
 //bind and listen the connections of the PORT
 app.listen(PORT, (err)=>{
     if (err) console.error("Error in server setup");
     console.log("Server listening on Port", PORT);
 });
+
 // transfers the file index.html to the path "/"
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
